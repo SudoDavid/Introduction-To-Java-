a33f38ef0ef26a09b4f8b0543a827b5a3ce794de
David Piro <36083906+SudoDavid@users.noreply.github.com>
2022-12-02 11:01:32 -0500

    Create Programming1.9.java

---
+++ Programming1.9.java
@@ -0,0 +1,13 @@
+class Triangle {
+    public static void main(String[] args) {
+        // Create Pyramid of 'T'
+        System.out.println("      ");
+        System.out.println("      T");
+        System.out.println("     TTT");
+        System.out.println("    TTTTT");
+        System.out.println("   TTTTTTT");
+        System.out.println("  TTTTTTTTT");
+        System.out.println(" TTTTTTTTTTT");
+        System.out.println("TTTTTTTTTTTTT");
+    }
+}
