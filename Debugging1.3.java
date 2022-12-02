c3295e6ac8b455d049257807d569f762620a1ce1
David Piro <36083906+SudoDavid@users.noreply.github.com>
2022-12-02 10:47:36 -0500

    Update Debugging1.3.java

--- Debugging1.3.java
+++ Debugging1.3.java
@@ -1,9 +1,9 @@
 public class DebugOne3
 {
-   public static void main(String args)
+   public static void main(String[] args)
    {
-      System.out.print1n("Over the river");
-      System.out.pr1ntln("and through the woods");
-      system.out.println("to Grandmother's house we go");
+      System.out.println("Over the river");
+      System.out.println("and through the woods");
+      System.out.println("to Grandmother's house we go");
    }
 }
