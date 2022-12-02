235c7e27b7a13d9e72a2be0978602cb2132c76a2
David Piro <36083906+SudoDavid@users.noreply.github.com>
2022-12-02 10:51:20 -0500

    Create Programming1.6.java

---
+++ Programming1.6.java
@@ -0,0 +1,9 @@
+class SongLyrics {
+    public static void main(String[] args) {
+        // Write your code here
+        System.out.println("Please run.");
+        System.out.println("Away far.");
+        System.out.println("And don't stop.");
+
+    }
+}
