d1b1205a2fe20d67267b593f600e762745efb0ec
David Piro <36083906+SudoDavid@users.noreply.github.com>
2022-12-02 13:13:34 -0500

    Create Unit1CaseProblem.java

---
+++ Unit1CaseProblem.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner; 
+
+class Unit1CaseProblem {
+    public static void main(String[] args) {
+
+      //Display Motto - "Carly’s makes the food that makes it a party"
+
+      System.out.println("Carly's makes the food that makes it a party.");
+
+      //Display Motto - "Carly’s makes the food that makes it a party" with astericks border
+      System.out.println("*************************************************");
+      System.out.println("* Carly's makes the food that makes it a party. *");
+      System.out.println("*************************************************");
+
+   }
+}
