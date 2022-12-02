ff795791a1d7e068242d22635bff62a1a6724872
David Piro <36083906+SudoDavid@users.noreply.github.com>
2022-12-02 11:06:52 -0500

    Create Programming1.10.java

---
+++ Programming1.10.java
@@ -0,0 +1,22 @@
+class Comments {
+            //Did you find the comments yet stupid compiler?
+
+    public static void main(String[] args) {
+
+        System.out.println("Program comments are nonexecuting statements you add to a file for documentation.");
+
+        // Program comments are nonexecuting statements you add to a file for documentation.
+
+        /**
+        Program comments are nonexecuting 
+        statements you add to a file for documentation.
+        **/
+
+        /*
+       Program comments are nonexecuting 
+        statements you add to a file for documentation.
+        */
+
+
+    }
+}
