f0bbde806b021ec51cf19bd275ceae733c53ff5a
David Piro <36083906+SudoDavid@users.noreply.github.com>
2022-12-02 11:15:34 -0500

    Update Programming1.11.java

--- Programming1.11.java
+++ Programming1.11.java
@@ -1,1 +1,25 @@
-temp
+import javax.swing.JOptionPane;
+import java.util.Scanner; 
+class CommentsDialog
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Program comments are nonexecuting statements ");
+      System.out.println("you add to a file for documentation.");
+      
+      JOptionPane.showMessageDialog(null,"Program comments are nonexecuting statements you add to a file for documentation.");
+
+      // three line comments
+      // Program comments are nonexecuting statements 
+      // you add to a file for documentation.
+      // one block comment
+      /* Program comments are nonexecuting statements 
+         you add to a file for documentation. */
+      // one javadoc comment
+      /** Program comments are nonexecuting statements 
+          you add to a file for documentation.  */
+   }
+}
+
+
+
