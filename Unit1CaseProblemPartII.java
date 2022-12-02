d6d9b6c564dc86576506bfcbcd61487235bf34cc
David Piro <36083906+SudoDavid@users.noreply.github.com>
2022-12-02 13:15:12 -0500

    Create Unit1CaseProblemPartII.java

---
+++ Unit1CaseProblemPartII.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner; 
+
+class Unit1CaseProblemPartII {
+    public static void main(String[] args) {
+
+      //Display Motto - "Carly’s makes the food that makes it a party"
+
+      System.out.println("Sammy’s makes it fun in the sun.");
+
+      //Display Motto - "Carly’s makes the food that makes it a party" with astericks border
+      System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+      System.out.println("S Sammy’s makes it fun in the sun. S");
+      System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+
+   }
+}
