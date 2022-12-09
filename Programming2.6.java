d11f9eb74e95173109c21583f7a46e0391503b16
David Piro <36083906+SudoDavid@users.noreply.github.com>
2022-12-08 22:56:50 -0500

    Create Programming2.6.java

---
+++ Programming2.6.java
@@ -0,0 +1,16 @@
+class MileConversions {
+    public static void main(String[] args) {
+        // Write your code here
+            final double INCHES_IN_MILE = 63360.0;
+            final double FEET_IN_MILE = 5280.0;
+            final double YARDS_IN_MILE = 1760;
+            int miles = 4;
+
+        System.out.println("4.0 miles is " + (miles * INCHES_IN_MILE) +
+      " inches or " + (miles * FEET_IN_MILE) +
+      " feet or " + (miles * YARDS_IN_MILE) +
+      " yards ");
+
+
+    }
+}
