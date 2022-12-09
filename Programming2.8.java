ba399ef2c11b38002d22e66c00d8c48ea4e09ea1
David Piro <36083906+SudoDavid@users.noreply.github.com>
2022-12-08 23:03:00 -0500

    Create Programming2.8.java

---
+++ Programming2.8.java
@@ -0,0 +1,11 @@
+class InchesToFeet {
+    public static void main(String[] args) {
+        // Write your code here
+        int inches = 86;
+        System.out.println("86 inches is " + (inches / 12) +
+      " feet and " + (inches % 12) +
+      " inches.");
+
+        
+    }
+}
