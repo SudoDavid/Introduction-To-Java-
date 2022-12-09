818f9f29d489c73a738a9d62cdc34a96fee8c452
David Piro <36083906+SudoDavid@users.noreply.github.com>
2022-12-08 22:36:18 -0500

    Create Programming2.4.java

---
+++ Programming2.4.java
@@ -0,0 +1,12 @@
+// QuartsToGallons.java
+class QuartsToGallons {
+    public static void main(String[] args) {
+        // Write your code here
+    final int QUARTS_IN_GALLON = 4;
+    int quartsNeeded = 18;
+
+    System.out.println("A job that needs " + quartsNeeded +
+      " quarts requires " + (quartsNeeded / 4) + " gallons plus " + (quartsNeeded % 4) + " quarts");
+
+    }
+}
