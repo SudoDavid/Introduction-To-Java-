a7acd7e5dc1bddafa3ca2400937a16ebbcbd3cd7
David Piro <36083906+SudoDavid@users.noreply.github.com>
2022-12-08 23:33:00 -0500

    Create Programming2.10.java

---
+++ Programming2.10.java
@@ -0,0 +1,14 @@
+class Initials {
+    public static void main(String[] args) {
+        // Write your code here
+
+        char init1;
+        init1 = 'J';
+        char init2;
+        init2 = 'M';
+        char init3;
+        init3 = 'F';
+
+        System.out.println(init1+"."+init2+"."+init3+".");
+    }
+}
